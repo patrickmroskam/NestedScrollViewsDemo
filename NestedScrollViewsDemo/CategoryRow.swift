431e7d7950b8a686ad749935f7d52ad1261b9052
Patrick M. Roskam <pat.roskam@gmail.com>
2018-04-12 19:36:43 -0700

    tested

---
+++ NestedScrollViewsDemo/CategoryRow.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryRow.swift
+//  NestedScrollViewsDemo
+//
+//  Created by Patrick Roskam on 4/12/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class CategoryRow : UITableViewCell {
+    
+}
+
+extension CategoryRow : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath as IndexPath)
+        return cell
+    }
+  
+}
+
+extension CategoryRow : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow:CGFloat = 4
+        let hardCodedPadding:CGFloat = 5
+        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+}
--- NestedScrollViewsDemo/ViewController.swift
+++ NestedScrollViewsDemo/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+    var catagories = ["Mountains", "Desert", "Ocean", "Forest", "People", "Animals", "Buildings", "Plants"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return catagories.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return catagories[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CategoryRow
+        return cell
+    }
 
 }
 
